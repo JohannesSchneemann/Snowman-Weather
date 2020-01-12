b56cf2b01bf81a51d73723345039a146063c0913
Johannes Schneemann <hannes.schneemann@gmail.com>
2020-01-12 13:51:56 -0600

    initial commit
    setup project files

---
+++ Snowman-Weather/Snowman-Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snowman-Weather
+//
+//  Created by Johannes Schneemann on 1/12/20.
+//  Copyright © 2020 Johannes Schneemann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
